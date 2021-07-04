31c897ba2f643879a3e6f2ad428bf6c2bc44f4ca
manosanjay-g <manosanjay.web@gmail.com>
2021-07-04 19:26:26 +0530

    added current_predictions list

--- home/views.py
+++ home/views.py
@@ -4,4 +4,33 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home/index.html')
+    current_predictions = [
+        {
+            'name': 'Mano ',
+            'batsman': 'Faf',
+            'bowler': 'Bumrah',
+            'pp1': 40,
+            'pp2': 40,
+            'score': 240,
+            'win': 'CSK'
+        },
+        {
+            'name': 'Krishna',
+            'batsman': 'Faf',
+            'bowler': 'Bumrah',
+            'pp1': 40,
+            'pp2': 40,
+            'score': 240,
+            'win': 'MI'
+        },
+        {
+            'name': 'Mithilesh',
+            'batsman': 'Faf',
+            'bowler': 'Bumrah',
+            'pp1': 40,
+            'pp2': 40,
+            'score': 240,
+            'win': 'CSK'
+        },
+    ]
+    return render(request, 'home/index.html', {'current_predictions': current_predictions})
